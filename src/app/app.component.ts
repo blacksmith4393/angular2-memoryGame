94d02f69cd75084e0e74279a7e400c75768dc7e1
blacksmith4393 <zach.smith33@gmail.com>
2016-12-11 21:47:37 -0500

    first commit and create intial tile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+export class Tile {
+  id: number;
+  name: string;
+  class: string;
+}
+
+const TILES: Tile[] = [
+  { id: 1, name:'android', class: "material-icons md-48"},
+  { id: 2, name: 'alarm', class: "material-icons md-48"},
+  { id: 3, name: 'cloud', class: "material-icons md-48" },
+  { id: 4, name: 'delete', class: "material-icons md-48" },
+  { id: 5, name: 'android', class: "material-icons md-48" },
+  { id: 6, name: 'alarm', class: "material-icons md-48" },
+  { id: 7, name: 'cloud', class: "material-icons md-48" },
+  { id: 8, name: 'delete', class: "material-icons md-48"}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  tiles = TILES;
+
+  selectedTile: Tile;
+
+  onSelect(tile: Tile): void {
+    this.selectedTile = tile;
+  }
+
 }
